061b4e3182ac11c6ef39cf64de4ceddeb7fd068d
a1m1ng <51517520+a1m1ng@users.noreply.github.com>
2024-03-01 14:22:13 +0200

    [lab 2][task 2] Add task file.

--- lab2/code/index.php
+++ lab2/code/index.php
@@ -3,6 +3,7 @@
 /* Imagine a lot of code here */
 $very_bad_unclear_name = "15 chicken wings";
 
+
 // Write your code here:
 
 $order =& $very_bad_unclear_name;
@@ -10,3 +11,20 @@ $order .= " python top";
 
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
+
+// Task 2
+
+$intVar = 42;
+echo $intVar;
+echo "\n";
+
+$floatVar = 13.45;
+echo $floatVar;
+echo "\n";
+
+echo 6 + 6;  // Result: 12
+echo "\n";
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo "Разница в расходах: " . ($last_month - $this_month);
