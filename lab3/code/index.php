628395e51d3bb05f508dd549be8a23446f6220dd
a1m1ng <51517520+a1m1ng@users.noreply.github.com>
2024-03-16 20:35:07 +0200

    [lab 3][task 1] Add task file.

--- lab3/code/index.php
+++ lab3/code/index.php
@@ -1,2 +1,19 @@
 <?php
-echo phpinfo();
+// Task 1
+// a)
+$str = 'ahb acb aeb aeeb adcb axeb';
+$matches = [];
+preg_match_all('/a..b/', $str, $matches);
+echo json_encode($matches[0]) . "\n";
+
+// b)
+$str = 'a1b2c3';
+
+function cube($matches) {
+    return pow($matches[0], 3);
+}
+
+$result = preg_replace_callback('/[0-9]+/', 'cube', $str);
+
+echo $result;
+?>
